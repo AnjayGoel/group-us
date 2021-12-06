462416dadaa0834c7e5de2d72cb269adea599bc4
AnjayGoel <angoel23@gmail.com>
2021-12-07 01:16:52 +0530

    migrate server

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ let api_url = null
 if (process.env.NODE_ENV === 'development') {
     api_url = "http://127.0.0.1:5000"
 } else {
-    api_url = "https://silverbug.eastus.cloudapp.azure.com"
+    api_url = "https://silverbugci.centralindia.cloudapp.azure.com"
 }
 export {api_url}
